8b82a561fbf918b4cd1933f9324d31d692f4481a
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-12-25 00:33:42 +0530

    modify 217. Contains Duplicate

--- Study Plan/Data Structure I/217. Contains Duplicate.py
+++ Study Plan/Data Structure I/217. Contains Duplicate.py
@@ -1,3 +1,6 @@
+# Problem Link: https://leetcode.com/problems/contains-duplicate/
 class Solution:
     def containsDuplicate(self, nums):
         return (len(nums) > len(set(nums)))
+
+# Alternate Method
