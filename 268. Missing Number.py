016413a0a3128927bdd0722c83b42c48c90d2d13
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-12-25 00:42:46 +0530

    add 268. Missing Number

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -1,1 +1,9 @@
 # Problem Link: https://leetcode.com/problems/missing-number/
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        for i in range(len(nums)+1):
+            if i in nums:
+                pass
+            else:
+                return i
