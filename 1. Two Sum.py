dd98dcd0ccca609cd99f858d78f012546efb61a2
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-12-25 23:48:47 +0530

    add 1. Two Sum

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,1 +1,11 @@
 # Problem Statement: https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, n in enumerate(nums):
+            m = target - n
+            if m in d:
+                return [d[m], i]
+            else:
+                d[n] = i
