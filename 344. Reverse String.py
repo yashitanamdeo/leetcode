ef1ac541963d9ddec628409db025fedbb58e586b
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-12-26 20:24:02 +0530

    add 344. Reverse String

--- 344. Reverse String.py
+++ 344. Reverse String.py
@@ -1,1 +1,25 @@
 # Problem Statement: https://leetcode.com/problems/reverse-string/
+
+# Method 1
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        start = 0
+        end = len(s) - 1
+        
+        while start < end:
+            s[start], s[end] = s[end], s[start]
+            
+            start += 1
+            end -= 1
+        return s
+
+# Method 2
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s[:] = s[::-1]
