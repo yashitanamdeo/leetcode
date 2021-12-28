c46b16f0b292a4ca671d3a6be7d604aefb7309a2
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-12-28 22:56:21 +0530

    update 1. Two Sum

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,5 +1,13 @@
 # Problem Statement: https://leetcode.com/problems/two-sum/
 
+# General Solution : Complexity - O(n^2)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return i,j
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = {}
