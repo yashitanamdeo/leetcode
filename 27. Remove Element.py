5271e2a0f9adbf2e02598d72bef749bd1945218f
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-12-29 00:35:15 +0530

    Add 27. Remove Element

--- 27. Remove Element.py
+++ 27. Remove Element.py
@@ -1,1 +1,14 @@
 # Problem Statement: https://leetcode.com/problems/remove-element/
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+
+        if len(nums) == 0:
+            return 0
+        else:
+            lp = 0
+            for rp in range(0,len(nums)):
+                if nums[rp] != val:
+                    nums[lp] = nums[rp]
+                    lp += 1
+            return lp
