d6c5aa18b4db381472a2c17c47da0cfd97c20d1a
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-12-28 23:25:24 +0530

    update 1. Two Sum

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -11,9 +11,19 @@ class Solution:
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = {}
-        for i, n in enumerate(nums):
-            m = target - n
-            if m in d:
-                return [d[m], i]
+        for index, value in enumerate(nums):
+            required_num = target - nums[index]
+            if required_num in d:
+                return [d[required_num],index] 
             else:
-                d[n] = i
+                d[value] = index
+
+'''
+Hum ek empty dictionary banayenge, 
+phir enumerate se index and values ajayegi nums array ki 
+then hum check karenge ki target - index or target - nums[index] kya hai 
+That is required number. Agar woh already dictionary me hai then we will return it
+warna we will put (eg) d[2] = 0 matlab 2:0
+Phir jab iske sath ka target hoga then we will return d[2] i.e 0 and index means 
+us time ka present index ie. 1
+'''
