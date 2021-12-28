7838b2d16aa0a4bd383cc825b58fa9fef25ebec3
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-12-29 00:47:11 +0530

    Add 27. Remove Element

--- 27. Remove Element.py
+++ 27. Remove Element.py
@@ -12,3 +12,11 @@ class Solution:
                     nums[lp] = nums[rp]
                     lp += 1
             return lp
+
+'''
+Similar to question 26. Remove duplicate element from sorted array
+Isme bas hume nums[rp-1] ki jagah us value se compare karna hai ki
+Kya hamara right pointer ki position wali value question ki value jisey remove karna hai usey different hai?
+Agar hai toh hum us different value ko left pointer ki jagah par store kardenge 
+This will automatically remove question wali value.
+'''
