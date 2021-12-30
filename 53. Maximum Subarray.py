902fd8f2ed6464028756a5d5f1a67b3342cfa642
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-12-30 23:50:20 +0530

    add 53. Maximum Subarray

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -1,1 +1,13 @@
 # Problem Statement: https://leetcode.com/problems/maximum-subarray/
+
+
+# Brute Force Solution : Complexity - O(n^2)
+class Solution:
+    def maxSubArray(self, nums):
+        ans = -inf
+        for i in range(len(nums)):
+            cur_sum = 0
+            for j in range(i, len(nums)):
+                cur_sum += nums[j]
+                ans = max(ans, cur_sum)
+        return ans
