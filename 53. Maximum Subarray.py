204c21bcae151442f817ced2cb8eefcaede716a4
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-12-31 21:13:46 +0530

    updated code

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -11,3 +11,5 @@ class Solution:
                 cur_sum += nums[j]
                 ans = max(ans, cur_sum)
         return ans
+
+""" https://leetcode.com/problems/maximum-subarray/discuss/1595195/C%2B%2BPython-7-Simple-Solutions-w-Explanation-or-Brute-Force-%2B-DP-%2B-Kadane-%2B-Divide-and-Conquer """
