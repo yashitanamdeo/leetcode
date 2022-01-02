4cc63f7e70b9f738637e220ecde41222e8f74565
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-02 21:42:08 +0530

    update 1010. Pairs of Songs with Total durations divisible by 60

--- 1010. Pairs of Songs with Total durations divisible by 60.py
+++ 1010. Pairs of Songs with Total durations divisible by 60.py
@@ -9,3 +9,26 @@ class Solution:
                 if (time[i] + time[j]) % 60 == 0:
                     count += 1
         return count
+
+# Using HashMap i.e dictionary : Complexity - O(n)
+class Solution:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        HashMap = {}
+        pairs = 0
+        
+        for t in time:
+            numMod = t % 60
+            
+            if numMod == 0:
+                if 0 in HashMap:
+                    pairs += HashMap[0]
+            elif (60 - numMod) in HashMap:
+                pairs += HashMap[60 - numMod]
+                
+            if numMod in HashMap:
+                HashMap[numMod] += 1
+            else:
+                HashMap[numMod] = 1
+                
+        return pairs
+                
