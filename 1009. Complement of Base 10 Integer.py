c94469bcab25cdf21fc1e126514b8ff766b46f7b
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-04 21:08:13 +0530

    add 1009. Complement of Base 10 Integer

--- 1009. Complement of Base 10 Integer.py
+++ 1009. Complement of Base 10 Integer.py
@@ -1,1 +1,15 @@
 ## Problem Statement: https://leetcode.com/problems/complement-of-base-10-integer/
+
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        if n == 0:
+            return 1
+        else:
+            result = 0
+            factor = 1
+            
+            while(n > 0):
+                result += factor * (1 if n%2 == 0 else 0)
+                factor *= 2
+                n //= 2
+            return result
