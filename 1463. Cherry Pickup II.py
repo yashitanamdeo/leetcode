d34a9a2305a43b5c751af0af900b95de131f6bd2
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-08 18:50:26 +0530

    add 1463. Cherry Pickup II

--- 1463. Cherry Pickup II.py
+++ 1463. Cherry Pickup II.py
@@ -1,1 +1,18 @@
 # Problem Statement: https://leetcode.com/problems/cherry-pickup-ii/
+
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+
+        @lru_cache(None)
+        def dfs(r, c1, c2):
+            if r == m: return 0
+            cherries = grid[r][c1] if c1 == c2 else grid[r][c1] + grid[r][c2]
+            ans = 0
+            for nc1 in range(c1 - 1, c1 + 2):
+                for nc2 in range(c2 - 1, c2 + 2):
+                    if 0 <= nc1 < n and 0 <= nc2 < n:
+                        ans = max(ans, dfs(r + 1, nc1, nc2))
+            return ans + cherries 
+
+        return dfs(0, 0, n - 1)
