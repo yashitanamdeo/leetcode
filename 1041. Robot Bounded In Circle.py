27745b2bbbf898a34400c8f6c377ca9b36829710
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-09 23:20:08 +0530

    add 1041. Robot Bounded In Circle

--- 1041. Robot Bounded In Circle.py
+++ 1041. Robot Bounded In Circle.py
@@ -1,1 +1,13 @@
 # Problem Statement: https://leetcode.com/problems/robot-bounded-in-circle/
+
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        x,y,dx,dy = 0,0,0,1
+        for i in instructions:
+            if i == 'G':
+                x,y = x+dx,y+dy
+            if i == 'L':
+                dx,dy = -dy, dx
+            if i == 'R':
+                dx,dy = dy, -dx
+        return (x,y) == (0,0) or (dx,dy) != (0,1)
