15383d2e0b9d471697f1c26ffa72aa87f949ba0e
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-15 23:51:32 +0530

    add 1345. Jump Game IV

--- 1345. Jump Game IV.py
+++ 1345. Jump Game IV.py
@@ -1,1 +1,42 @@
 # Problem Statement: https://leetcode.com/problems/jump-game-iv/
+
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        visited = set()
+        xs = defaultdict(list)
+        for i, v in enumerate(arr):
+            xs[v].append(i)
+        
+        N = len(arr)
+        if N == 1:
+            return 0
+
+        
+        num_steps = 1
+        level = [N-1]
+        visited.add(N-1)
+
+        while level:
+            next_level = set()
+            # print(level, num_steps)
+            
+            for i in level:
+                if i == 1:
+                    return num_steps
+                
+                if i-1 not in visited:
+                    visited.add(i-1)
+                    next_level.add(i-1)
+                if i != N-1 and i+1 not in visited:
+                    visited.add(i+1)
+                    next_level.add(i+1)
+                
+                for ni in xs[arr[i]]:
+                    if ni == 0:
+                        return num_steps
+                    if ni not in visited:
+                        visited.add(ni)
+                        next_level.add(ni)
+            
+            num_steps += 1
+            level = next_level
