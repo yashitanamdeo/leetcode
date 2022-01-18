39f66d5ae36c663271a7e02c3c4ef294c77ea782
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-18 23:52:22 +0530

    add 605. Can Place Flowers

--- 605. Can Place Flowers.py
+++ 605. Can Place Flowers.py
@@ -1,1 +1,11 @@
 # Problem Statement: https://leetcode.com/problems/can-place-flowers/
+
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        if n == 0: return True
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0):  # can place?
+                n -= 1
+                if n == 0: return True
+                flowerbed[i] = 1  # palce!
+        return False
