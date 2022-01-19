ceaa43fa7dd53645ed93a42727a4ffd0b9b79f0d
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-19 23:03:20 +0530

    update 53. Maximum Subarray

--- Easy/53. Maximum Subarray.py
+++ Easy/53. Maximum Subarray.py
@@ -4,12 +4,11 @@
 # Brute Force Solution : Complexity - O(n^2)
 class Solution:
     def maxSubArray(self, nums):
-        ans = -inf
-        for i in range(len(nums)):
-            cur_sum = 0
-            for j in range(i, len(nums)):
-                cur_sum += nums[j]
-                ans = max(ans, cur_sum)
-        return ans
+        dp = [[0]*len(nums) for i in range(2)]
+        dp[0][0], dp[1][0] = nums[0], nums[0]
+        for i in range(1, len(nums)):
+            dp[1][i] = max(nums[i], nums[i] + dp[1][i-1])
+            dp[0][i] = max(dp[0][i-1], dp[1][i])
+        return dp[0][-1]
 
 """ https://leetcode.com/problems/maximum-subarray/discuss/1595195/C%2B%2BPython-7-Simple-Solutions-w-Explanation-or-Brute-Force-%2B-DP-%2B-Kadane-%2B-Divide-and-Conquer """
