e4ca47d30a46b039c6ed1fc8be7e8f74aa723fef
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-23 23:44:42 +0530

    add 1291. Sequential Digits

--- Medium/1291. Sequential Digits.py
+++ Medium/1291. Sequential Digits.py
@@ -1,1 +1,16 @@
 # Problem Statement: https://leetcode.com/problems/sequential-digits/
+
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        result = list()
+
+        for window in range(floor(log10(low)) + 1, floor(log10(high)) + 2):
+            for start in range(10 - window):
+                number = 0
+                for i in range(start, start + window):
+                    number = number * 10 + i + 1
+
+                if low <= number <= high: 
+                    result.append(number)
+
+        return result
