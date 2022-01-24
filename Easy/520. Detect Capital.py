99d34c4167dec80fbf68e513a3ae01226f7ae9c0
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-24 19:36:34 +0530

    update 520. Detect Capital

--- Easy/520. Detect Capital.py
+++ Easy/520. Detect Capital.py
@@ -6,3 +6,10 @@ class Solution:
         if word.islower(): return True
         if word[0].isupper() and word[1:].islower(): return True
         return False
+
+# Alternate Solution
+class Solution(object):
+    def detectCapitalUse(self, word):
+        return word.isupper() or word.islower() or word.istitle()
+
+# Post: https://leetcode.com/problems/detect-capital/discuss/1715987/Python-Easy
