aa0c4ddca46bd189efc5e93c170e759c9c978400
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-24 19:31:37 +0530

    add 520. Detect Capital

--- Easy/520. Detect Capital.py
+++ Easy/520. Detect Capital.py
@@ -1,1 +1,8 @@
 # Problem Statement: https://leetcode.com/problems/detect-capital/
+
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word.isupper(): return True
+        if word.islower(): return True
+        if word[0].isupper() and word[1:].islower(): return True
+        return False
