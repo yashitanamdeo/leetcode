8333b607f29956ea1701e1b6879f626fd2b341de
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-26 23:20:05 +0530

    add 1305. add Elements in Two Binary Search Trees

--- Medium/1305. All Elements in Two Binary Search Trees.py
+++ Medium/1305. All Elements in Two Binary Search Trees.py
@@ -6,24 +6,3 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
-        st1, st2 = [], []
-        res = []
-        
-        while root1 != None or root2 != None or len(st1) > 0 or len(st2) > 0:
-            while root1 != None:
-                st1.append(root1)
-                root1 = root1.left
-            while root2 != None:
-                st2.append(root2)
-                root2 = root2.left
-            if len(st2) == 0 or (len(st1) > 0 and st1[-1].val <= st2[-1].val):
-                root1 = st1.pop()
-                res.append(root1.val)
-                root1 = root1.right
-            else:
-                root2 = st2.pop()
-                res.append(root2.val)
-                root2 = root2.right
-        return res
