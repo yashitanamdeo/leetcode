87eebe308b4227c97506974340605587c1794550
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-30 23:24:30 +0530

    add 189. Rotate Array

--- Medium/189. Rotate Array.py
+++ Medium/189. Rotate Array.py
@@ -1,1 +1,9 @@
 # Problem Statement: https://leetcode.com/problems/rotate-array/
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
