c54e22b2153b255b96b6b6fc083b16899756ef33
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-30 23:25:11 +0530

    update 189. Rotate Array

--- Medium/189. Rotate Array.py
+++ Medium/189. Rotate Array.py
@@ -7,3 +7,30 @@ class Solution:
         """
         k = k % len(nums)
         nums[:] = nums[-k:] + nums[:-k]
+
+# Alternate Solution
+'''
+class Solution:
+    def reverse (self, nums, i, j) : 
+        li = i
+        ri = j
+        
+        while li < ri:
+            temp = nums[li]
+            nums[li] = nums[ri]
+            nums[ri] = temp
+            
+            li += 1
+            ri -= 1
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        if k < 0 : 
+            k += len(nums)
+        
+        self.reverse(nums, 0, len(nums) - k - 1);
+        self.reverse(nums, len(nums) - k, len(nums) - 1);
+        self.reverse(nums, 0, len(nums) - 1);
+'''
