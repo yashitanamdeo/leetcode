f79b67ae127fcc39e041118a300cfb0d9a468b85
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-31 18:43:25 +0530

    add 1672. Richest Customer Wealth

--- Easy/1672. Richest Customer Wealth.py
+++ Easy/1672. Richest Customer Wealth.py
@@ -1,1 +1,11 @@
 # Problem Statement: https://leetcode.com/problems/richest-customer-wealth/
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        max_wealth = []
+        for i in range(len(accounts)):
+            wealth = 0
+            for j in range(len(accounts[i])):
+                wealth += accounts[i][j]
+                max_wealth.append(wealth)
+        return max(max_wealth)
