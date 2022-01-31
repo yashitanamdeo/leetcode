5c831566a57dea410cd272b008e82f6d2fb0170e
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-31 18:50:43 +0530

    add 1672. Richest Customer Wealth

--- Easy/1672. Richest Customer Wealth.py
+++ Easy/1672. Richest Customer Wealth.py
@@ -9,3 +9,15 @@ class Solution:
                 wealth += accounts[i][j]
                 max_wealth.append(wealth)
         return max(max_wealth)
+
+        # return max(sum(acc) for acc in accounts)
+        # return max(map(sum, accounts))
+
+# Alternate Solution
+'''
+def maximumWealth(self, accounts: List[List[int]]) -> int:
+        wealth = []
+        for i in accounts:
+            wealth.append(sum(i))
+        return max(wealth)
+'''
