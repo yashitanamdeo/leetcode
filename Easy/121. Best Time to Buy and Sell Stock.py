8c9e1a17be66ae6fba14b3f6858168622712e62b
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-01 23:14:00 +0530

    add 121. Best Time to Buy and Sell Stock

--- Easy/121. Best Time to Buy and Sell Stock.py
+++ Easy/121. Best Time to Buy and Sell Stock.py
@@ -1,1 +1,15 @@
 # Problem Statement: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = 0
+        sell = 1
+        max_profit = 0
+        while sell < len(prices):
+            currentProfit = prices[sell] - prices[buy]
+            if prices[buy] < prices[sell]:
+                max_profit = max(currentProfit,max_profit)
+            else:
+                buy = sell
+            sell += 1
+        return max_profit
