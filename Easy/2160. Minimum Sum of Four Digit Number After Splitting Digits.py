62e5d2ac5f82adce0fff22596048e0825aa84f14
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-05 22:07:59 +0530

    add 2160. Minimum Sum of Four Digit Number After Splitting Digits

--- Easy/2160. Minimum Sum of Four Digit Number After Splitting Digits.py
+++ Easy/2160. Minimum Sum of Four Digit Number After Splitting Digits.py
@@ -1,1 +1,18 @@
 # Problem Statement: https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/
+
+class Solution:
+    def minimumSum(self, num: int) -> int:
+        x = [int(a) for a in str(num)]
+        mini = min(x)
+        x.remove(min(x))
+        smini = min(x)
+        x.remove(min(x))
+        #print(mini,smini)
+        #print(x)
+        mini = str(mini) + str(x[0])
+        num1 = int(mini)
+        #print(num1)
+        smini = str(smini) + str(x[1])
+        num2 = int(smini)
+        #print(num2)
+        return num1 + num2
