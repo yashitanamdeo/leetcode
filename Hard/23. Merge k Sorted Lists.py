65abbcb382bfb883f2b6cdba2807f902b40bb31a
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-05 22:38:45 +0530

    add 23. Merge k Sorted Lists

--- Hard/23. Merge k Sorted Lists.py
+++ Hard/23. Merge k Sorted Lists.py
@@ -1,1 +1,25 @@
 # Problem Statement: https://leetcode.com/problems/merge-k-sorted-lists/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        if not lists:
+            return None
+        if len(lists) == 1:
+            return lists[0]
+        mid = len(lists) // 2
+        l, r = self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:])
+        return self.merge(l, r)
+    
+    def merge(self, l, r):
+        if not l or not r:
+            return l or r
+        if l.val< r.val:
+            l.next = self.merge(l.next, r)
+            return l
+        r.next = self.merge(l, r.next)
+        return r
