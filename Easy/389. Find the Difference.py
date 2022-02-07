c2970756c3db306969c687c74ab855391c85736c
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-07 14:57:41 +0530

    add 389. Find the Difference

--- Easy/389. Find the Difference.py
+++ Easy/389. Find the Difference.py
@@ -1,1 +1,7 @@
 # Problem Statement: https://leetcode.com/problems/find-the-difference/
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        for letter in t:
+            if t.count(letter) != s.count(letter):
+                return letter
