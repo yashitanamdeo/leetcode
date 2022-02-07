ab189969547a643135adaeb1ea8ea7f4432b7724
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-07 15:09:14 +0530

    update 389. Find the Difference

--- Easy/389. Find the Difference.py
+++ Easy/389. Find the Difference.py
@@ -5,3 +5,18 @@ class Solution:
         for letter in t:
             if t.count(letter) != s.count(letter):
                 return letter
+
+# Alternate Solution
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        if len(s) == 0:
+            return t
+        
+        s = sorted(s)
+        t = sorted(t)
+        
+        for index, s_val in enumerate(s):
+            if t[index] != s_val:
+                return t[index]
+        
+        return t[-1]
