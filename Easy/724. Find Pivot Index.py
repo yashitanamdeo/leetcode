7681278f150715b136a27b79263819153a7c1756
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-07 23:14:46 +0530

    add 724. Find Pivot Index

--- Easy/724. Find Pivot Index.py
+++ Easy/724. Find Pivot Index.py
@@ -1,1 +1,11 @@
 # Problem Statement: https://leetcode.com/problems/find-pivot-index/
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        leftSum, rightSum = 0,sum(nums)
+        for index,num in enumerate(nums):
+            rightSum -= num
+            if rightSum == leftSum:
+                return index
+            leftSum += num
+        return -1
