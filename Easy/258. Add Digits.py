692a1e65daaf574bfad4b4b0a7aa6faae9d9524e
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-08 20:45:28 +0530

    add 258. Add Digits

--- Easy/258. Add Digits.py
+++ Easy/258. Add Digits.py
@@ -1,1 +1,10 @@
 # Problem Statement: https://leetcode.com/problems/add-digits/
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0: 
+            return 0
+        if num % 9 == 0: 
+            return 9
+        else: 
+            return(num % 9)
