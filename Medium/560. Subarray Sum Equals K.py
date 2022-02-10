6ca5b6eb28e19a120a238c1afd7a43628f190c05
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-10 22:36:15 +0530

    add 560. Subarray Sum Equals K

--- Medium/560. Subarray Sum Equals K.py
+++ Medium/560. Subarray Sum Equals K.py
@@ -1,1 +1,11 @@
 # Problem Statement: https://leetcode.com/problems/subarray-sum-equals-k/
+
+# Solution 1 - Complexity: O(n^2) [Time Limit Exceeded]
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        for i in range(len(nums)):
+            for j in range(i,len(nums)):
+                if sum(nums[i:j+1]) == k:
+                    count += 1
+        return count
