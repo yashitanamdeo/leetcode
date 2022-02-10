1ece25da449603bff1ed53ff02eb344fd43c0536
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-10 22:40:26 +0530

    add 560. Subarray Sum Equals K

--- Medium/560. Subarray Sum Equals K.py
+++ Medium/560. Subarray Sum Equals K.py
@@ -9,3 +9,42 @@ class Solution:
                 if sum(nums[i:j+1]) == k:
                     count += 1
         return count
+
+# Solution 2 - Complexity: O(n) [Accepted]
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        d = {}
+        d[0] = 1
+        s = 0
+        count = 0
+        for i in range(len(nums)):
+            s += nums[i]
+            if s-k in d: # --- I
+                count += d[s-k]
+                # or return True
+                # or return indicies
+           
+            # add sum to frq dict
+            if s in d:
+                d[s] += 1 # --- II
+            else:
+                d[s] = 1
+       
+        return count
+   	    
+   	
+   	   # COMMENT -- I
+   	   # ---------------
+   	   # Single scan. Given the current sum and the k, we check if (sum-k) existed as previous sum at an earlier stage ( aka smaller window size)
+   	   # Keep expanding the sum while checking whether we have seen (sum - k) before
+   	
+       
+       
+       # COMMENT -- II
+       # ---------------
+       # It's possible that the freq of a sum could be greater than 1 only when the nums list conatins a zero
+       # ex: nums = [1,2,3,0,4]
+       # because sum will be the same for two consecutive iterations.
+       # it's important to capture this edge case in order to return the correct number of subarrays that
+       # add up to target.
+       # if we are guaranteed that the list nums has no zeros, then we can replace the prefix dict with a prefix set
