0d812b0df98009c3148ca1bdcf32d91c164feab6
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-11 23:41:52 +0530

    add 567. Permutation in String

--- Medium/567. Permutation in String.py
+++ Medium/567. Permutation in String.py
@@ -1,1 +1,23 @@
 # Problem Statement: https://leetcode.com/problems/permutation-in-string/
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        '''One string is a permutation of another if the two strings
+       have the same character frequencies
+       
+       Hence:
+           - find freq dict for s1
+           - find freq dict for substrings of s2 (that are the same size as s1)
+           
+       runtime: O(n*k^2)
+       '''
+       
+        from collections import Counter
+        d1 = Counter(s1)
+        k = len(s1)
+        for i in range(len(s2)):  # ---- O(n)
+            sub = s2[i:i+k]  # ------ O(k)
+            d2 = Counter(sub) # --- O(k)
+            if d1 == d2:
+                return True
+        return False
