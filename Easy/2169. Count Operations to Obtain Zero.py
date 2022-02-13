f0910920e335cbf686e2a58aa13db91284b45ce4
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-13 21:45:32 +0530

    add 2169. Count Operations to Obtain Zero

--- Easy/2169. Count Operations to Obtain Zero.py
+++ Easy/2169. Count Operations to Obtain Zero.py
@@ -1,1 +1,14 @@
 # Problem Statement: https://leetcode.com/problems/count-operations-to-obtain-zero/
+
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        count = 0
+        #while(num1 > 0 and num2 > 0):
+        while(min(num1,num2) > 0):
+            if num1 >= num2:
+                num1 -= num2
+                count += 1
+            else:
+                num2 -= num1
+                count += 1
+        return count    
