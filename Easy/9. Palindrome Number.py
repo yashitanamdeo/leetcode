df0bd5b81bd09c67c954773f825c63b4304ec22b
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-13 22:23:54 +0530

    add 9. Palindrome Number

--- Easy/9. Palindrome Number.py
+++ Easy/9. Palindrome Number.py
@@ -1,1 +1,8 @@
 # Problem Statement: https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        return str(x) == str(x)[::-1]
