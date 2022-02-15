e2d451d545d2d6f40fdf4f56fa29951c537d770e
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-15 23:29:41 +0530

    update 136. Single Number

--- Easy/136. Single Number.py
+++ Easy/136. Single Number.py
@@ -5,3 +5,11 @@ class Solution:
         for num in nums:
             if nums.count(num) == 1:
                 return num
+
+# Alternative Solution
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for i in range(len(nums)):
+            result ^= nums[i]
+        return result
