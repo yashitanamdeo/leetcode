c096c2598ec7dd5121d39817e4620618810028c4
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-15 23:26:00 +0530

    add 136. Single Number

--- Easy/136. Single Number.py
+++ Easy/136. Single Number.py
@@ -1,1 +1,7 @@
 # Problem Statement: https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        for num in nums:
+            if nums.count(num) == 1:
+                return num
