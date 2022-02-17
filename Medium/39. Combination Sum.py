b481140a87cae14ea2e41bd63ea2ddd2164e0cee
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-17 18:02:57 +0530

    add 39. Combination Sum

--- Medium/39. Combination Sum.py
+++ Medium/39. Combination Sum.py
@@ -1,1 +1,18 @@
 # Problem Statement: https://leetcode.com/problems/combination-sum/
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()  # sort to terminate early when target < 0
+        
+        def backtracking(i, target, path):
+            if target == 0:
+                ans.append(path)
+                return
+            if i == len(candidates) or target < candidates[i]:
+                return
+            backtracking(i, target - candidates[i], path + [candidates[i]]) # Choose ith candidate
+            backtracking(i + 1, target, path) # Skip ith candidate
+        
+        ans = []
+        backtracking(0, target, [])
+        return ans
