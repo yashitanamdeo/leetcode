44b24f4a98476d405b53eab3d60a8248eb8f1431
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-18 23:54:05 +0530

    add 402. Remove K Digits

--- Medium/402. Remove K Digits.py
+++ Medium/402. Remove K Digits.py
@@ -1,1 +1,13 @@
 # Problem Statement: https://leetcode.com/problems/remove-k-digits/
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:    
+        stack = []
+        for digit in num:
+            while k > 0 and len(stack) > 0 and stack[-1] > digit:
+                k -= 1
+                stack.pop()  
+            stack.append(digit)
+        if k > 0:
+            stack = stack[:-k]     
+        return "".join(stack).lstrip("0") or "0"
