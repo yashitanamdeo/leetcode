f3e3e53b5657fc4821d76e3c90fb46c730903c13
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-19 13:55:08 +0530

    add 1675. Minimize Deviation in Array

--- Hard/1675. Minimize Deviation in Array.py
+++ Hard/1675. Minimize Deviation in Array.py
@@ -1,1 +1,16 @@
 # Problem Statement: https://leetcode.com/problems/minimize-deviation-in-array/
+
+class Solution:
+        def minimumDeviation(self, A):
+            pq = []
+            for a in A:
+                heapq.heappush(pq, [a / (a & -a), a])
+            res = float('inf')
+            ma = max(a for a, a0 in pq)
+            while len(pq) == len(A):
+                a, a0 = heapq.heappop(pq)
+                res = min(res, ma - a)
+                if a % 2 == 1 or a < a0:
+                    ma = max(ma, a * 2)
+                    heapq.heappush(pq, [a * 2, a0])
+            return int(res)
