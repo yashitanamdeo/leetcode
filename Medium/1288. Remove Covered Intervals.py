ba35a1f3b38380f9d26739ffeb7d0d9425e1a9b7
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-20 12:49:43 +0530

    add 1288. Remove Covered Intervals

--- Medium/1288. Remove Covered Intervals.py
+++ Medium/1288. Remove Covered Intervals.py
@@ -1,1 +1,16 @@
 # Problem Statement: https://leetcode.com/problems/remove-covered-intervals/
+
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        start, end = 0, 1
+        # Sort by left bound, and reversed right bound of interval
+        intervals.sort(key = lambda segment:(segment[start], -segment[end]) )
+        # count of non-covered intervals
+        count = 0
+		# right bound so far
+        right_bound = 0
+        for interval in intervals:
+            if right_bound < interval[end]:
+                # previous interval can not cover current interval, update count
+                count, right_bound = count + 1, interval[end]
+        return count
