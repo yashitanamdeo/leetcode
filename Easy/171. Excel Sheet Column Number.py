7cde292c104eb913fe61b3ddb28bfd70c71720fc
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-22 23:56:56 +0530

    add 171. Excel Sheet Column Number

--- Easy/171. Excel Sheet Column Number.py
+++ Easy/171. Excel Sheet Column Number.py
@@ -1,1 +1,8 @@
 # Problem Statement: https://leetcode.com/problems/excel-sheet-column-number/
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        answer = 0
+        for char in columnTitle:
+            answer = answer*26 + ord(char)-ord('A')+1
+        return answer
