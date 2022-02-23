b511d6d0d0985e1d4894c6731a8872d5cb062f14
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-23 22:16:05 +0530

    add 133. Clone Graph

--- Medium/133. Clone Graph.py
+++ Medium/133. Clone Graph.py
@@ -1,1 +1,65 @@
 # Problem Statement: https://leetcode.com/problems/clone-graph/
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    # BFS
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return 
+        nodeCopy = Node(node.val, [])
+        dic = {node: nodeCopy}
+        queue = collections.deque([node])
+        while queue:
+            node = queue.popleft()
+            for neighbor in node.neighbors:
+                if neighbor not in dic: # neighbor is not visited
+                    neighborCopy = Node(neighbor.val, [])
+                    dic[neighbor] = neighborCopy
+                    dic[node].neighbors.append(neighborCopy)
+                    queue.append(neighbor)
+                else:
+                    dic[node].neighbors.append(dic[neighbor])
+        return nodeCopy
+# DFS iteratively
+    def cloneGraph(self, node):
+        if not node:
+            return 
+        nodeCopy = Node(node.val, [])
+        dic = {node: nodeCopy}
+        stack = [node]
+        while stack:
+            node = stack.pop()
+            for neighbor in node.neighbors:
+                if neighbor not in dic:
+                    neighborCopy = Node(neighbor.val, [])
+                    dic[neighbor] = neighborCopy
+                    dic[node].neighbors.append(neighborCopy)
+                    stack.append(neighbor)
+                else:
+                    dic[node].neighbors.append(dic[neighbor])
+        return nodeCopy
+# DFS recursively
+    def cloneGraph(self, node):
+        if not node:
+            return 
+        nodeCopy = Node(node.val, [])
+        dic = {node: nodeCopy}
+        self.dfs(node, dic)
+        return nodeCopy
+
+    def dfs(self, node, dic):
+        for neighbor in node.neighbors:
+            if neighbor not in dic:
+                neighborCopy = Node(neighbor.val, [])
+                dic[neighbor] = neighborCopy
+                dic[node].neighbors.append(neighborCopy)
+                self.dfs(neighbor, dic)
+            else:
+                dic[node].neighbors.append(dic[neighbor])
