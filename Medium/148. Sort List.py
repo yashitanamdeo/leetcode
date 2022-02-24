f9c86ce9bd13f483b7dc9914dadd9cbc1a047bbd
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-24 23:28:56 +0530

    update 148. Sort List

--- Medium/148. Sort List.py
+++ Medium/148. Sort List.py
@@ -1,1 +1,48 @@
 # Problem Statement: https://leetcode.com/problems/sort-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def merge(self, h1, h2):
+        dummy = tail = ListNode(None)
+        while h1 and h2:
+            if h1.val < h2.val:
+                tail.next, h1 = h1, h1.next
+            else:
+                tail.next, h2 = h2, h2.next
+            tail = tail.next
+    
+        tail.next = h1 or h2
+        return dummy.next
+    
+    def sortList(self, head):
+        if not head or not head.next:
+            return head
+    
+        pre, slow, fast = None, head, head
+        while fast and fast.next:
+            pre, slow, fast = slow, slow.next, fast.next.next
+        pre.next = None
+
+        return self.merge(*map(self.sortList, (head, slow)))
+
+# Alternate Solution
+class Solution(object):
+    def sortList(self, head):
+        if not head:
+            return None
+        values = []
+        node = head
+        while node:
+            values.append(node.val)
+            node = node.next
+        values.sort()
+        #print(values)
+        node = head
+        for n in values:
+            node.val = n
+            node = node.next
+        return head
