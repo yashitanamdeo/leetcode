10f3fbed8bc714af363cfe6735b55191d9b4879a
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-27 22:08:10 +0530

    add 2185. Counting Words With a Given Prefix

--- Easy/2185. Counting Words With a Given Prefix.py
+++ Easy/2185. Counting Words With a Given Prefix.py
@@ -1,1 +1,9 @@
 # Problem Statement: https://leetcode.com/problems/counting-words-with-a-given-prefix/
+
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        count = 0
+        for word in words:
+            if pref in word[:len(pref)]:
+                count += 1
+        return count
