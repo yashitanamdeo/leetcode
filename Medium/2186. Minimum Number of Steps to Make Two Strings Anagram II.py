84208d5365b756f8204ca1474005f2e7374cab79
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-27 22:06:58 +0530

    add 2186. Minimum Number of Steps to Make Two Strings Anagram II

--- Medium/2186. Minimum Number of Steps to Make Two Strings Anagram II.py
+++ Medium/2186. Minimum Number of Steps to Make Two Strings Anagram II.py
@@ -1,1 +1,17 @@
 # Problem Statement: https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram-ii/
+
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        count = 0
+        char_count = [0] * 26
+        for i in range(26):
+            char_count[i] = 0
+        for i in range(len(s)):
+            char_count[ord(s[i]) - ord('a')] += 1
+        for i in range(len(t)):
+            char_count[ord(t[i]) - ord('a')] -= 1
+        for i in range(26):
+            if char_count[i] != 0:
+                if char_count[i] != 0:
+                    count += abs(char_count[i])
+        return count
