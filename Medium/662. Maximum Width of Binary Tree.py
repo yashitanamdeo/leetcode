61df6ae349516adf6003ba3368c6c3d2dfeb1df4
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-27 21:33:40 +0530

    add 662. Maximum Width of Binary Tree

--- Medium/662. Maximum Width of Binary Tree.py
+++ Medium/662. Maximum Width of Binary Tree.py
@@ -1,1 +1,33 @@
 # Problem Statement: https://leetcode.com/problems/maximum-width-of-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def widthOfBinaryTree(self, root):
+        if not root: return []
+        width, level = 0, [(root, 1)]
+        # Keep going untill current level has some nodes.
+        while len(level):
+            # Put all children of current level in next_level.
+            width = max(width, level[-1][1] - level[0][1] + 1)
+            next_level = []
+            for item, num in level:
+                if item.left:   # Make sure to not put the Null nodes
+                    next_level.append((item.left, num*2))
+                if item.right:
+                    next_level.append((item.right, num*2+1))
+            level = next_level
+        return width
+
+'''
+That's numbering nodes (and nulls) like this:
+
+                  1
+          2               3
+      4       5       6       7
+    8   9   ...
+'''
