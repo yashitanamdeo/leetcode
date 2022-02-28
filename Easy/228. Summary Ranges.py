06622cb1f43ef39e4c0316ebede12496fc7de1a8
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-28 23:07:16 +0530

    add 228. Summary Ranges

--- Easy/228. Summary Ranges.py
+++ Easy/228. Summary Ranges.py
@@ -1,1 +1,20 @@
 # Problem Statement: https://leetcode.com/problems/summary-ranges/
+
+class Solution:
+	def summaryRanges(self, nums: List[int]) -> List[str]:
+		start = 0
+		end = 0
+		result = []
+		while start < len(nums) and end<len(nums):
+			if end+1 < len(nums) and nums[end]+1 == nums[end+1]:
+				end=end+1
+			else:
+				if start == end:
+					result.append(str(nums[start]))
+					start = start + 1
+					end = end + 1
+				else:
+					result.append(str(nums[start])+'->'+str(nums[end]))
+					start = end + 1
+					end = end + 1
+		return result
