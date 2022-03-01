06bf927c6b41b66a98848404644d8e1dec7ddb19
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-01 23:23:32 +0530

    add 338. Counting Bits

--- Easy/338. Counting Bits.py
+++ Easy/338. Counting Bits.py
@@ -1,1 +1,20 @@
 # Problem Statement: https://leetcode.com/problems/counting-bits/
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        def toBinary(num):
+             return bin(num).replace("0b", "")
+        
+        binary_num = []
+        for i in range(n+1):
+            num = toBinary(i)
+            binary_num.append(str(num))
+        
+        answer = []
+        for element in binary_num:
+            count = 0
+            for digit in element:
+                if digit == '1':
+                    count += 1
+            answer.append(count)
+        return answer
