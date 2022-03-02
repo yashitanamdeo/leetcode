cc6d01d68dd4e0e599b9bbb1d811453ac57f4908
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-02 22:53:04 +0530

    add 392. Is Subsequence

--- Easy/392. Is Subsequence.py
+++ Easy/392. Is Subsequence.py
@@ -1,1 +1,14 @@
 # Problem Statement: https://leetcode.com/problems/is-subsequence/
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) > len(t):return False #if len of s is greater than len of t, return false because s cant be a subsequence of t
+        if len(s) == 0:return True
+        subsequence=0
+        for i in range(0,len(t)):
+            if subsequence <= len(s) -1: #if it is matching, increment subsequence
+                print(s[subsequence])
+                if s[subsequence]==t[i]:
+
+                    subsequence+=1
+        return  subsequence == len(s) 
