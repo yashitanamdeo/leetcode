d591e3a97610c2b658df079544718d44f0c90364
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-03 15:45:42 +0530

    add 1281. Subtract the Product and Sum of Digits of an Integer

--- Easy/1281. Subtract the Product and Sum of Digits of an Integer.py
+++ Easy/1281. Subtract the Product and Sum of Digits of an Integer.py
@@ -1,1 +1,12 @@
 # Problem Statement: https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        product_of_digits = 1
+        sum_of_digits = 0
+        n = str(n)
+        for digit in n:
+            product_of_digits *= int(digit)
+            sum_of_digits += int(digit)
+        
+        return product_of_digits - sum_of_digits
