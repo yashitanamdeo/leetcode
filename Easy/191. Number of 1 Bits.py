dcd6037c62aae71b658dc78406be1258d15ecb48
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-03 15:39:13 +0530

    update 191. Number of 1 Bits

--- Easy/191. Number of 1 Bits.py
+++ Easy/191. Number of 1 Bits.py
@@ -3,3 +3,22 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         return bin(n).count('1')
+
+# Alternate Solution
+class Solution:
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        c = 0
+        while n:
+            n &= n - 1
+            c += 1
+        return c
+    
+'''
+Using bit operation to cancel a 1 in each round
+
+Think of a number in binary n = XXXXXX1000, n - 1 is XXXXXX0111. n & (n - 1) will be XXXXXX0000 which is just remove the last significant 1
+'''
