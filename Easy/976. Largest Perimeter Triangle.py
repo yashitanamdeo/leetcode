f846c7340b3ac05f043a056a6148ced57640cc94
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-04 23:24:05 +0530

    update 976. Largest Perimeter Triangle

--- Easy/976. Largest Perimeter Triangle.py
+++ Easy/976. Largest Perimeter Triangle.py
@@ -7,3 +7,11 @@ class Solution:
             if(A[i-3] < A[i-2] + A[i-1]):
                 return sum(A[i-3:i])
         return 0
+
+'''
+1. Sort the List to get the top 3 lengths
+2. Check if the largest length is less than sum of other two
+    * If 2 is false, drop the max length take next 3 largest length and repeat 1-2
+    * if 2 is true, return sum of all lengths
+3. if loop ends, and no possible combination found, return 0
+'''
