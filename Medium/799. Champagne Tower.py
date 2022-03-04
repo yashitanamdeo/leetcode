bd2412d82267bb814768c40fe26b034d2fec05c2
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-04 23:20:56 +0530

    add 799. Champagne Tower

--- Medium/799. Champagne Tower.py
+++ Medium/799. Champagne Tower.py
@@ -1,1 +1,17 @@
 # Problem Statement: https://leetcode.com/problems/champagne-tower/
+
+class Solution:
+    def champagneTower(self, poured, query_row, query_glass):
+        l = [poured]
+        for _ in range(query_row):
+            l_new = [0] * (len(l) + 1)
+            for i in range(len(l)):
+                pour = (l[i] - 1)/2
+                if pour > 0:
+                    l_new[i] += pour
+                    l_new[i+1] += pour
+            
+            l = l_new
+                    
+        return min(1, l[query_glass])
+
