a7748eb670d2441cebf19e74d756738639169520
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-04 23:21:25 +0530

    update 799. Champagne Tower

--- Medium/799. Champagne Tower.py
+++ Medium/799. Champagne Tower.py
@@ -15,3 +15,9 @@ class Solution:
                     
         return min(1, l[query_glass])
 
+'''
+What we need to do in this problem is to simulate our pouring process: imagine, that we have 6 cups of water arrived at some place, then one full cup is left on this place and 2.5 cups go to the right bottom and left bottom cups. What we need to do now is to simulate this process!
+
+1. We start with l = [poured] - top layer
+2. Then we process full current layer to create next layer. We need to check if we have enough champagne: if we have less than 1, this cup is dead-end. If it has more than 1, then we add values to bottom layer.
+'''
