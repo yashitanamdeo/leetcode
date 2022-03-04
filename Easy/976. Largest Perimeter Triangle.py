876298f7e04f56a943c2190a58f0453e2fb0a348
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-04 23:22:37 +0530

    add 976. Largest Perimeter Triangle

--- Easy/976. Largest Perimeter Triangle.py
+++ Easy/976. Largest Perimeter Triangle.py
@@ -1,1 +1,9 @@
 # Problem Statement: https://leetcode.com/problems/largest-perimeter-triangle/
+
+class Solution:
+    def largestPerimeter(self, A: List[int]) -> int:
+        A.sort(reverse = True)
+        for i in range(3,len(A)+1):
+            if(A[i-3] < A[i-2] + A[i-1]):
+                return sum(A[i-3:i])
+        return 0
