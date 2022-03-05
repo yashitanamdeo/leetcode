4940d1d1aca182305e84ab5e6831c2e14e427ede
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-06 00:58:11 +0530

    add 1790. Check if One String Swap Can Make Strings Equal

--- Easy/1790. Check if One String Swap Can Make Strings Equal.py
+++ Easy/1790. Check if One String Swap Can Make Strings Equal.py
@@ -1,1 +1,8 @@
 # Problem Statement: https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/
+
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        count = 0
+        for ch1, ch2 in zip(s1, s2):
+            if ch1 != ch2: count += 1
+        return (Counter(s1) == Counter(s2)) and (count == 0 or count == 2)
