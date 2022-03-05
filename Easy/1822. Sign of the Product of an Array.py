147f74db4f06209330f5699624b0b0a40d7ba95f
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-06 00:20:34 +0530

    add 1822. Sign of the Product of an Array

--- Easy/1822. Sign of the Product of an Array.py
+++ Easy/1822. Sign of the Product of an Array.py
@@ -1,1 +1,14 @@
 # Problem Statement: https://leetcode.com/problems/sign-of-the-product-of-an-array/
+
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        product = 1
+        for i in range(len(nums)):
+            product *= nums[i]
+        
+        if product == 0:
+            return 0
+        elif product > 0:
+            return 1
+        else:
+            return -1
