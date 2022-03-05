59d5310f7c86477b9b3b705d54d63d2e0cfdc209
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-06 00:51:26 +0530

    add 202. Happy Number

--- Easy/202. Happy Number.py
+++ Easy/202. Happy Number.py
@@ -1,1 +1,14 @@
 # Problem Statement: https://leetcode.com/problems/happy-number/
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        while(n > 6 and n!=1):
+            squareSum = 0
+            num = str(n)
+            for i in range(len(num)):
+                squareSum += int(num[i])**2
+            n = squareSum
+            
+        if n == 1:
+            return True
+        return False
