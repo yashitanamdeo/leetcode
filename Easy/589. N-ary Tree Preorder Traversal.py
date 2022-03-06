3a8dab1d7cab3a9dbe6352f89752e57a20caf7a2
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-06 23:49:57 +0530

    add 589. N-ary Tree Preorder Traversal

--- Easy/589. N-ary Tree Preorder Traversal.py
+++ Easy/589. N-ary Tree Preorder Traversal.py
@@ -1,1 +1,39 @@
 # Problem Statement: https://leetcode.com/problems/n-ary-tree-preorder-traversal/
+
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if root is None:
+            return []
+        
+        stack = [root]
+        output = []
+        
+        # Till there is element in stack the loop runs.
+        while stack:
+            
+            #pop the last element from the stack and store it into temp.
+            temp = stack.pop()
+            
+            # append. the value of temp to output
+            output.append(temp.val)
+            
+            #add the children of the temp into the stack in reverse order.
+            # children of 1 = [3,2,4], if not reveresed then 4 will be popped out first from the stack.
+            # if reversed then stack = [4,2,3]. Here 3 will pop out first.
+            # This continues till the stack is empty.
+            stack.extend(temp.children[::-1])
+        
+        #return the output
+        return output
