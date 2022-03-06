c77d155d8cdb644f52c7771a1541619a0f7a66cb
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-06 23:44:09 +0530

    add 1359. Count All Valid Pickup and Delivery Options

--- Hard/1359. Count All Valid Pickup and Delivery Options.py
+++ Hard/1359. Count All Valid Pickup and Delivery Options.py
@@ -1,1 +1,10 @@
 # Problem Statement: 1359. Count All Valid Pickup and Delivery Options
+
+from functools import reduce
+from operator import mul
+class Solution:
+    def countOrders(self, n: int) -> int:
+        cap = 10**9 + 7
+        pickup_permutation = math.factorial(n) % cap
+        delivery_permutation = reduce(mul, range(1, 2*n, 2), 1) % cap
+        return pickup_permutation * delivery_permutation % cap
