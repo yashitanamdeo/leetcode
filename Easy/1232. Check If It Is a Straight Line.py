c9cc60438d96666e20818cb79df6dad4f5cceea4
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-07 00:12:58 +0530

    add 1232. Check If It Is a Straight Line

--- Easy/1232. Check If It Is a Straight Line.py
+++ Easy/1232. Check If It Is a Straight Line.py
@@ -1,1 +1,10 @@
 # Problem Statement: https://leetcode.com/problems/check-if-it-is-a-straight-line/
+
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        (x1, y1), (x2, y2) = coordinates[:2]
+        for i in range(2, len(coordinates)):
+            (x, y) = coordinates[i]
+            if((y2 - y1) * (x1 - x) != (y1 - y) * (x2 - x1)):
+                return False
+        return True		
