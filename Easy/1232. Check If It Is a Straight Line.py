b8ae9c617b2e044708c7e0f695415deb2cedbfc9
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-07 00:13:29 +0530

    update 1232. Check If It Is a Straight Line

--- Easy/1232. Check If It Is a Straight Line.py
+++ Easy/1232. Check If It Is a Straight Line.py
@@ -8,3 +8,10 @@ class Solution:
             if((y2 - y1) * (x1 - x) != (y1 - y) * (x2 - x1)):
                 return False
         return True		
+
+'''
+The point is if we take points p1(x, y), p2(x1, y1), p3(x3, y3), slopes of any two pairs is same then p1, p2, p3 lies on same line.
+slope from p1 and p2 is y - y1 / x - x1
+slope from p2 and p3 is y2 - y1 / x2 - x1
+if these two slopes equal, then p1, p2, p3 lies on same line.
+'''
