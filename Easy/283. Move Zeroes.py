a959525f6c2d53d9169884261bebf215b5afc213
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-07 23:51:34 +0530

    add 283. Move Zeroes

--- Easy/283. Move Zeroes.py
+++ Easy/283. Move Zeroes.py
@@ -1,1 +1,13 @@
 # Problem Statement: https://leetcode.com/problems/move-zeroes/
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        last0 = 0
+        for i in range(0,len(nums)):
+            if (nums[i]!=0):
+                nums[i],nums[last0] = nums[last0],nums[i]
+                last0+=1
+        #nums.sort(key=bool, reverse=True) 
