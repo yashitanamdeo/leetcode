dac822a44d0df07b6776809c7d2de202b61b72a8
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-07 23:50:51 +0530

    add 283. Move Zeroes

--- Easy/21. Merge Two Sorted Lists.py
+++ Easy/21. Merge Two Sorted Lists.py
@@ -1,1 +1,20 @@
 # Problem Statement: https://leetcode.com/problems/merge-two-sorted-lists/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = cur = ListNode(0)
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1 = list1.next
+            else:
+                cur.next = list2
+                list2 = list2.next
+            cur = cur.next
+        cur.next = list1 or list2
+        return dummy.next
