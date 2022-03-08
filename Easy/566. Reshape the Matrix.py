91338d2743da30188fb7e871d6596c9676b28fcc
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-08 23:20:17 +0530

    add 566. Reshape the Matrix

--- Easy/566. Reshape the Matrix.py
+++ Easy/566. Reshape the Matrix.py
@@ -1,1 +1,17 @@
 # Problem Statement: https://leetcode.com/problems/reshape-the-matrix/
+
+class Solution:
+    def matrixReshape(self,nums, r, c):
+        flat_list = []
+        matrix = []
+
+        for sublist in nums:
+            for item in sublist:
+                flat_list.append(item)
+
+        if len(flat_list) != r * c:
+            return nums
+        else:
+            for i in range(0,len(flat_list),c):
+                matrix.append(flat_list[i:i+c])
+            return matrix
