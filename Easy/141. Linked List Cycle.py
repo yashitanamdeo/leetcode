d5b7545517b893a95aea81fedcac6d685e823494
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-08 22:59:37 +0530

    add 141. Linked List Cycle

--- Easy/141. Linked List Cycle.py
+++ Easy/141. Linked List Cycle.py
@@ -1,1 +1,19 @@
 # Problem Statement: https://leetcode.com/problems/linked-list-cycle/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head):
+        try:
+            slow = head
+            fast = head.next
+            while slow is not fast:
+                slow = slow.next
+                fast = fast.next.next
+            return True
+        except:
+            return False
