198e4d30af075d8d5ee8d5b893fdf3eccfc8c1f6
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-16 22:39:50 +0530

    add 946. Validate Stack Sequences

--- Medium/946. Validate Stack Sequences.py
+++ Medium/946. Validate Stack Sequences.py
@@ -1,1 +1,11 @@
 # Problem Statement: https://leetcode.com/problems/validate-stack-sequences/
+
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        index, stack = 0, []
+        for p in pushed:
+            stack.append(p)
+            while stack and stack[-1] == popped[index]:
+                stack.pop()
+                index += 1
+        return not stack
