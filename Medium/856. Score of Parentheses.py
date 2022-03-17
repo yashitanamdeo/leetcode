52e55b66b690b665ee23d4c81c0629e1ede17360
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-17 22:25:03 +0530

    add 856. Score of Parentheses

--- Medium/856. Score of Parentheses.py
+++ Medium/856. Score of Parentheses.py
@@ -1,1 +1,13 @@
 # Problem Statement: https://leetcode.com/problems/score-of-parentheses/
+
+class Solution(object):
+    def scoreOfParentheses(self, s):
+        stack = []
+        answer = 0
+        for c in s:
+            if c == '(':
+                stack.append(answer)
+                answer = 0
+            else:
+                answer = stack.pop() + max(1, answer*2)
+        return answer 
