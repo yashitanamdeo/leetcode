72c85dd4e564d9277c798099fd701f2604d3189c
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-19 22:22:11 +0530

    add 2206. Divide Array Into Equal Pairs

--- Easy/2206. Divide Array Into Equal Pairs.py
+++ Easy/2206. Divide Array Into Equal Pairs.py
@@ -1,1 +1,11 @@
 # Problem Statement: https://leetcode.com/problems/divide-array-into-equal-pairs/
+
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+		#counting appearances
+        count = collections.Counter(nums)
+		#if a number has an odd frequency, it can't be split as the constraints require, so we can just return false
+        for k, v in count.items():
+            if v % 2:
+                return False
+        return True  
