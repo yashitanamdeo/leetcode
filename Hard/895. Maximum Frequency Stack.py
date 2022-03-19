a322967c2bdee004fd20d48fdd61faf54c4458e1
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-19 18:50:17 +0530

    add 895. Maximum Frequency Stack

--- Hard/895. Maximum Frequency Stack.py
+++ Hard/895. Maximum Frequency Stack.py
@@ -17,7 +17,9 @@ class FreqStack:
         self.m[toBeRemoved[2]]-=1
         return toBeRemoved[2]
         
-
+''' Heap will always keep the element with max count on top, and if two elements have same count, 
+the second element (index) will be considered while doing pop operation. 
+Also, the count map, is useful when the new occurence of the exisiting element is pushed.'''
 
 # Your FreqStack object will be instantiated and called as such:
 # obj = FreqStack()
