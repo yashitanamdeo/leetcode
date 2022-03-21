117e160f5c5f5d4f27b82e6ad8fccf4297a00b3b
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-21 23:01:36 +0530

    add 763. Partition Labels

--- Medium/763. Partition Labels.py
+++ Medium/763. Partition Labels.py
@@ -1,1 +1,18 @@
 # Problem Statement: https://leetcode.com/problems/partition-labels/
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        
+        last = {s[i]: i for i in range(len(s))} # last appearance of the letter
+        i, ans = 0, []
+        while i < len(s):
+            end, j = last[s[i]], i + 1
+            while j < end: # validation of the part [i, end]
+                if last[s[j]] > end:
+                    end = last[s[j]] # extend the part
+                j += 1
+           
+            ans.append(end - i + 1)
+            i = end + 1
+            
+        return ans
