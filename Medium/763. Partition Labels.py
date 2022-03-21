02e98298f07b7643e734a4778f61fff796c54533
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-21 23:02:39 +0530

    update 763. Partition Labels

--- Medium/763. Partition Labels.py
+++ Medium/763. Partition Labels.py
@@ -16,3 +16,15 @@ class Solution:
             i = end + 1
             
         return ans
+    
+'''
+Since each letter can appear only in one part, we cannot form a part shorter than the 
+index of the last appearance of a letter subtracted by an index of the first appearance. 
+For example here (absfab) the lengths of the first part are limited by the positions of the letter a. 
+So it's important to know at what index each letter appears in the string last time. 
+We can create a hash map and fill it with the last indexes for letters.
+
+Also, we have to validate a candidate part. 
+For the same example (absfab) we see that letter a cannot form a border for the first part 
+because of a nasty letter b inside. So we need to expand the range of the initial part.
+'''
