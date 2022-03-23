9ab84e5d0185ca239dad29f8ceda25b5a2296a3b
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-23 23:43:50 +0530

    add 991. Broken Calculator

--- Medium/991. Broken Calculator.py
+++ Medium/991. Broken Calculator.py
@@ -1,1 +1,12 @@
 # Problem Statement: https://leetcode.com/problems/broken-calculator/
+
+class Solution:
+    def brokenCalc(self, X, Y):
+        if X > Y: return X - Y
+        if X == Y: return 0
+        if Y % 2 == 0:
+            return self.brokenCalc(X, Y//2) + 1
+        else:
+            return self.brokenCalc(X, Y + 1) + 1
+        
+    #https://leetcode.com/problems/broken-calculator/discuss/1076046/Python-Greedy-solution-explained
