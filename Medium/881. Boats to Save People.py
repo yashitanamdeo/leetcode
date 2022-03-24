3efb8062e89b00cde872089ca80add0bb17d22bc
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-24 22:53:06 +0530

    add 881. Boats to Save People

--- Medium/881. Boats to Save People.py
+++ Medium/881. Boats to Save People.py
@@ -1,1 +1,22 @@
 # Problem Statement: https://leetcode.com/problems/boats-to-save-people/
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        left, right, result = 0, len(people) - 1, 0
+        while left <= right:
+            if people[left] + people[right] <= limit:
+                left += 1
+            result += 1
+            right -= 1
+        return result
+'''
+Sort people.
+For the current heaviest person, we try to let him go with the lightest person.
+As all people need to get on the boat.
+If the sum of weight is over the limit, we have to let the heaviest go alone.
+No one can take the same boat with him.
+
+At the end of for loop, it may happend that i = j.
+But it won't change the result
+'''
