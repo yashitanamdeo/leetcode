43ff603797c62fcb402e92e2944f50cd9e8faeb2
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-24 23:22:08 +0530

    update 1768. Merge Strings Alternately

--- Easy/1768. Merge Strings Alternately.py
+++ Easy/1768. Merge Strings Alternately.py
@@ -17,3 +17,12 @@ class Solution:
                 ans = ans + word1[i] + word2[i]
             ans = ans + word1[i+1:]
             return ans
+        
+# Alternate Solution
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        mergedString = ''
+        for i in range(min(len(word1),len(word2))):
+            mergedString += word1[i] + word2[i]
+            
+        return mergedString + word1[i+1:] + word2[i+1:]
