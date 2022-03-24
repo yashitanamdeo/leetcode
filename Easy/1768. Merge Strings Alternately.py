f3077ee8247e24026080125a8349dd695d0b28d1
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-24 23:21:31 +0530

    add 1768. Merge Strings Alternately

--- Easy/1768. Merge Strings Alternately.py
+++ Easy/1768. Merge Strings Alternately.py
@@ -1,1 +1,19 @@
 # Problem Statement: https://leetcode.com/problems/merge-strings-alternately/
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        ans = ""
+        if len(word1) == len(word2):
+            for i in range(len(word1)):
+                ans = ans + word1[i] + word2[i]
+            return ans
+        elif len(word1) < len(word2):
+            for i in range(len(word1)):
+                ans = ans + word1[i] + word2[i]
+            ans = ans + word2[i+1:]
+            return ans
+        else:
+            for i in range(len(word2)):
+                ans = ans + word1[i] + word2[i]
+            ans = ans + word1[i+1:]
+            return ans
