293d013d7153b329d71c816b411932b7e8579574
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-24 23:59:25 +0530

    add 1678. Goal Parser Interpretation

--- Easy/1678. Goal Parser Interpretation.py
+++ Easy/1678. Goal Parser Interpretation.py
@@ -1,1 +1,13 @@
 # Problem Statement: https://leetcode.com/problems/goal-parser-interpretation/
+
+class Solution:
+    def interpret(self, s: str) -> str:
+        dic = {"(al)":"al", "()":"o","G":"G"}
+        tmp = ""
+        result = ""
+        for i in range(len(s)):
+            tmp += s[i]
+            if(tmp in dic):
+                result += dic[tmp]
+                tmp = ""
+        return result
