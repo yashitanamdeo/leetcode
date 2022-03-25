375d19b5bb079f2d16a9ebaf3c91c669a88350bc
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-26 00:09:13 +0530

    add 1029. Two City Scheduling

--- Medium/1029. Two City Scheduling.py
+++ Medium/1029. Two City Scheduling.py
@@ -1,1 +1,14 @@
 # Problem Statement: https://leetcode.com/problems/two-city-scheduling/
+
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        refund = []
+        N = len(costs)//2
+        minCost = 0
+        for A, B in costs:
+            refund.append(B - A)
+            minCost += A
+        refund.sort()
+        for i in range(N):
+            minCost += refund[i]
+        return minCost
