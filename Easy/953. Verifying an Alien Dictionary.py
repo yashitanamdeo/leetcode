a354e4e4e9a77c4e725a40a35c9e53f451209d7d
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-26 01:13:40 +0530

    add 953. Verifying an Alien Dictionary

--- Easy/953. Verifying an Alien Dictionary.py
+++ Easy/953. Verifying an Alien Dictionary.py
@@ -1,1 +1,23 @@
 # Problem Statement: https://leetcode.com/problems/verifying-an-alien-dictionary/
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        
+        for a, b in zip(words, words[1:]):
+            i, j, A, B = 0, 0, len(a), len(b)
+
+            while i < A and j < B:
+                aOrderIdx = order.index(a[i])
+                bOrderIdx = order.index(b[i])
+                if aOrderIdx == bOrderIdx:
+                    i += 1
+                    j += 1
+                elif aOrderIdx > bOrderIdx:
+                    return False
+                else:
+                    break
+
+            if i < A and j == B:
+                return False
+            
+        return True
