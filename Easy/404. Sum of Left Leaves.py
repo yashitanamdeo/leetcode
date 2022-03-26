a1e8bba79a7555e5730488162ae39be9f9da708f
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-26 21:06:04 +0530

    add 404. Sum of Left Leaves

--- Easy/404. Sum of Left Leaves.py
+++ Easy/404. Sum of Left Leaves.py
@@ -1,1 +1,14 @@
 # Problem Statement: https://leetcode.com/problems/sum-of-left-leaves/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        if not root: return 0
+        if root.left and not root.left.left and not root.left.right:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
