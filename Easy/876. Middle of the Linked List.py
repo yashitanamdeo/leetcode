976de80122cc289abf60cf72f0c7f46f93039345
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-26 20:58:38 +0530

    add 876. Middle of the Linked List

--- Easy/876. Middle of the Linked List.py
+++ Easy/876. Middle of the Linked List.py
@@ -1,1 +1,27 @@
 # Problem Statement: https://leetcode.com/problems/middle-of-the-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def middleNode(self, head):
+        # While slow moves one step forward, fast moves two steps forward.
+        # Finally, when fast reaches the end, slow happens to be in the middle of the linked list.
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    
+'''
+While slow moves one step forward, fast moves two steps forward.
+Finally, when fast reaches the end, slow happens to be in the middle of the linked list.
+For example, head = [1, 2, 3, 4, 5].
+    step 0: slow: [1, 2, 3, 4, 5], fast: [1, 2, 3, 4, 5]
+    step 1: slow: [1, 2, 3, 4, 5], fast: [1, 2, 3, 4, 5]
+    step 2: slow: [1, 2, 3, 4, 5], fast: [1, 2, 3, 4, 5]
+    end because fast cannot move forward anymore and return [3, 4, 5]
+'''
