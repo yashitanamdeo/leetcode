2be65c22e79b93d4f41763f38e5b116ba10bb1ca
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-26 20:50:09 +0530

    update 1290. Convert Binary Number in a Linked List to Integer

--- Easy/1290. Convert Binary Number in a Linked List to Integer.py
+++ Easy/1290. Convert Binary Number in a Linked List to Integer.py
@@ -13,3 +13,39 @@ class Solution:
             answer = 2 * answer + head.val 
             head = head.next 
         return answer 
+    
+    
+'''
+For Example:
+1101 -> 13
+
+Binary View
+0000 * 2 # shift left
+0000 + 1 # add bit
+
+0001 * 2 # shift left
+0010 + 1 # add bit
+
+0011 * 2 # shift left
+0110 + 0 # add bit
+
+0110 * 2 # shift left
+1100 + 1 # add bit
+
+1101 # 13
+
+Decimal View
+0 * 2 # x2
+0 + 1 # add bit
+
+1 * 2 # x2
+2 + 1 # add bit
+
+3 * 2 # x2
+6 + 0 # add bit
+
+6 * 2 # x2
+12 + 1 # add bit
+
+13 # Answer
+'''
