bb5d52e4722027273aaad2ee57b8f7147a11f5f2
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-28 00:15:36 +0530

    add 242. Valid Anagram

--- Easy/242. Valid Anagram.py
+++ Easy/242. Valid Anagram.py
@@ -1,1 +1,10 @@
 # Problem Statement: https://leetcode.com/problems/valid-anagram/
+
+class Solution:
+    def isAnagram(self, s, t):
+        dic1, dic2 = {}, {}
+        for item in s:
+            dic1[item] = dic1.get(item, 0) + 1
+        for item in t:
+            dic2[item] = dic2.get(item, 0) + 1
+        return dic1 == dic2
