a821f6e1c21b79a4e2a3bd35ac3982f86eb6bb1b
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-28 00:04:43 +0530

    add 1356. Sort Integers by The Number of 1 Bits

--- Easy/1356. Sort Integers by The Number of 1 Bits.py
+++ Easy/1356. Sort Integers by The Number of 1 Bits.py
@@ -1,1 +1,14 @@
 # Problem Statement: https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/
+
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        arr.sort()
+        brr=[]
+        for i in arr:
+            brr.append([i, bin(i).replace("0b","").count("1")])
+        brr.sort(key = lambda x: x[1])
+        ans=[]
+        for element, bit_count in brr:
+            ans.append(element)
+
+        return ans
