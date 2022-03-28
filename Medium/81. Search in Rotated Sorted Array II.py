705e9ead39f54f32f4a3c3e769a2c4d637aafb00
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-28 22:28:03 +0530

    add 81. Search in Rotated Sorted Array II

--- Medium/81. Search in Rotated Sorted Array II.py
+++ Medium/81. Search in Rotated Sorted Array II.py
@@ -1,1 +1,26 @@
 # Problem Statement: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+
+class Solution:
+    def search(self, nums, target):
+        l, r = 0, len(nums)-1
+        while l <= r:
+            mid = l + (r-l)//2
+            if nums[mid] == target:
+                return True
+            while l < mid and nums[l] == nums[mid]:
+                l += 1
+            # the first half is ordered
+            if nums[l] <= nums[mid]:
+                # target is in the first half
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            # the second half is ordered
+            else:
+                # target is in the second half
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return False
