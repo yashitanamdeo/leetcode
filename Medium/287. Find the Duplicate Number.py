ae16bb418978e80e9ae09bca08e309fca4e2318d
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-29 23:51:52 +0530

    add 287. Find the Duplicate Number

--- Medium/287. Find the Duplicate Number.py
+++ Medium/287. Find the Duplicate Number.py
@@ -1,1 +1,18 @@
 # Problem Statement: https://leetcode.com/problems/find-the-duplicate-number/
+
+class Solution(object):
+    def findDuplicate(self, nums):
+        low = 0
+        high = len(nums) - 1
+        mid = (high + low) // 2
+        while high - low > 1:
+            count = 0
+            for k in nums:
+                if mid < k <= high:
+                    count += 1
+            if count > high - mid:
+                low = mid
+            else:
+                high = mid
+            mid = (high + low) // 2
+        return high
