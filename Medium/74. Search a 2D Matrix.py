0b0d9783c3b0570a9e1dc980af0acf37bf85dfe4
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-30 23:44:21 +0530

    add 74. Search a 2D Matrix

--- Medium/74. Search a 2D Matrix.py
+++ Medium/74. Search a 2D Matrix.py
@@ -1,1 +1,18 @@
 # Problem Statement: https://leetcode.com/problems/search-a-2d-matrix/
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for i in range(len(matrix)):
+            if target == matrix[i][0]:
+                return True
+            if target < matrix[i][0]:
+                for j in range(len(matrix[i-1])):
+                    if target==matrix[i-1][j]:
+                        return True
+            elif target == matrix[i][len(matrix[i])-1]:
+                return True
+            elif target <= matrix[i][len(matrix[i])-1]:
+                for j in range(len(matrix[i])):
+                    if target==matrix[i][j]:
+                        return True
+        return False
