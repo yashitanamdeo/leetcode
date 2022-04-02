43bdd0aeba0c4ef90f8fa3aff4930d6387878ab7
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-04-02 23:30:59 +0530

    add 680. Valid Palindrome II

--- Easy/680. Valid Palindrome II.py
+++ Easy/680. Valid Palindrome II.py
@@ -1,1 +1,17 @@
 # Problem Statement: https://leetcode.com/problems/valid-palindrome-ii/
+
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s)-1
+        while i < j:
+            if s[i] != s[j]:
+                delete_i = s[i+1:j+1]
+                delete_j = s[i:j]
+                return self._isPalindrome(delete_i) or self._isPalindrome(delete_j)
+            i += 1
+            j -= 1
+        return True
+    
+    def _isPalindrome(self, s):
+        return s == s[::-1]
