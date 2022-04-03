856e609b6eab66576fc90c1012033f969c6af6c6
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-04-03 23:07:35 +0530

    add 31. Next Permutation

--- Medium/31. Next Permutation.py
+++ Medium/31. Next Permutation.py
@@ -1,1 +1,22 @@
 # Problem Statement: https://leetcode.com/problems/next-permutation/
+
+class Solution:
+    def nextPermutation(self, nums):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = j = len(nums)-1
+        while i > 0 and nums[i-1] >= nums[i]:
+            i -= 1
+        if i == 0:   # nums are in descending order
+            nums.reverse()
+            return 
+        k = i - 1    # find the last "ascending" position
+        while nums[j] <= nums[k]:
+            j -= 1
+        nums[k], nums[j] = nums[j], nums[k]  
+        l, r = k+1, len(nums)-1  # reverse the second part
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l +=1 ; r -= 1
+        
