1e7e1863eddb8a1d4752336d55b1d5fa5a27b8dc
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-04-07 00:01:30 +0530

    add 923. 3Sum With Multiplicity

--- Medium/923. 3Sum With Multiplicity.py
+++ Medium/923. 3Sum With Multiplicity.py
@@ -1,1 +1,17 @@
 # Problem Statement: https://leetcode.com/problems/3sum-with-multiplicity/
+
+class Solution:
+    def threeSumMulti(self, A, T):
+        nmap, third, ans = [0 for _ in range(101)], ceil(T / 3) - 1, 0
+        for num in A: nmap[num] += 1
+        for k in range(min(T,100), third, -1):
+            rem = T - k
+            half = ceil(rem / 2) - 1
+            for j in range(min(rem, k), half, -1):
+                i = rem - j
+                x, y, z = nmap[i], nmap[j], nmap[k]
+                if i == k: ans += x * (x-1) * (x-2) // 6
+                elif i == j: ans += x * (x-1) // 2 * z
+                elif j == k: ans += x * y * (y-1) // 2
+                else: ans += x * y * z
+        return ans % 1000000007
