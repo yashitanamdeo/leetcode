afe0e085b32a9d825050d9ce4b308b08d2bcb04c
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-04-16 23:55:07 +0530

    add 538. Convert BST to Greater Tree

--- Medium/538. Convert BST to Greater Tree.py
+++ Medium/538. Convert BST to Greater Tree.py
@@ -1,1 +1,28 @@
 # Problem Statement: https://leetcode.com/problems/convert-bst-to-greater-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def convertBST(self, root):
+        def visit1(root):
+            if root:
+                visit1(root.left)
+                vals.append(root.val)
+                visit1(root.right)
+        vals = []
+        visit1(root)
+
+        self.s = 0
+        def visit2(root):
+            if root:
+                visit2(root.right)
+                self.s += vals.pop()
+                root.val = self.s
+                visit2(root.left)
+        visit2(root)
+
+        return root
