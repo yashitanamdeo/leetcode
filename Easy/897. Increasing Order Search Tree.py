da5f1c0e411d650f58d00a40be58e78abcbcaef0
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-04-17 23:11:15 +0530

    add 897. Increasing Order Search Tree

--- Easy/897. Increasing Order Search Tree.py
+++ Easy/897. Increasing Order Search Tree.py
@@ -1,1 +1,25 @@
 # Problem Statement: https://leetcode.com/problems/increasing-order-search-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def increasingBST(self, root):
+        def dfs(node):
+            l1, r2 = node, node
+            
+            if node.left: 
+                l1, l2 = dfs(node.left)
+                l2.right = node
+                
+            if node.right:
+                r1, r2 = dfs(node.right)
+                node.right = r1
+            
+            node.left = None
+            return (l1, r2)
+        
+        return dfs(root)[0]
