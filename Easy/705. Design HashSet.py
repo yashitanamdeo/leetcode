643b48efa102c9c1c149b36c44f483ae68ab6c80
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-04-21 23:43:06 +0530

    add 705. Design HashSet

--- Easy/705. Design HashSet.py
+++ Easy/705. Design HashSet.py
@@ -1,1 +1,30 @@
 # Problem Statement: https://leetcode.com/problems/design-hashset/
+
+class MyHashSet: 
+    def eval_hash(self, key):
+        return ((key*1031237) & (1<<20) - 1)>>5
+
+    def __init__(self):
+        self.arr = [[] for _ in range(1<<15)]
+
+    def add(self, key: int) -> None:
+        t = self.eval_hash(key)
+        if key not in self.arr[t]:
+            self.arr[t].append(key)
+
+    def remove(self, key: int) -> None:
+        t = self.eval_hash(key)
+        if key in self.arr[t]:
+            self.arr[t].remove(key)
+
+    def contains(self, key: int) -> bool:
+        t = self.eval_hash(key)
+        return key in self.arr[t]
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
