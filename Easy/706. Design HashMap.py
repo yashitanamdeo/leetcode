7e47a4b5ffa07ad5a5081b6a25d5086f5a19835a
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-04-23 00:03:46 +0530

    add 706. Design HashMap

--- Easy/706. Design HashMap.py
+++ Easy/706. Design HashMap.py
@@ -1,1 +1,23 @@
 # Problem Statement: https://leetcode.com/problems/design-hashmap/
+
+class MyHashMap:
+    def __init__(self):
+        self.data = [None] * 1000001
+        
+    def put(self, key: int, val: int) -> None:
+        self.data[key] = val
+        
+    def get(self, key: int) -> int:
+        val = self.data[key]
+        return val if val != None else -1
+    
+    def remove(self, key: int) -> None:
+        self.data[key] = None
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
