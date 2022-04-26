b96c3f03a0c8e562b1ee9a57395895b9fddbd3ab
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-04-26 23:53:39 +0530

    add 1584. Min Cost to Connect All Points

--- Medium/1584. Min Cost to Connect All Points.py
+++ Medium/1584. Min Cost to Connect All Points.py
@@ -1,1 +1,11 @@
 # Problem Statement: https://leetcode.com/problems/min-cost-to-connect-all-points/
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        d, res = {(x, y): float('inf') if i else 0 for i, (x, y) in enumerate(points)}, 0
+        while d:
+            x, y = min(d, key=d.get)  # obtain the current minimum edge
+            res += d.pop((x, y))      # and remove the corresponding point
+            for x1, y1 in d:          # for the rest of the points, update the minimum manhattan distance
+                d[(x1, y1)] = min(d[(x1, y1)], abs(x-x1)+abs(y-y1))
+        return res
