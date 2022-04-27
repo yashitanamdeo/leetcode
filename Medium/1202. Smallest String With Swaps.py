16b955f025382877c9cc80a99f15b0174f6c05ca
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-04-27 23:35:17 +0530

    add 1202. Smallest String With Swaps

--- Medium/1202. Smallest String With Swaps.py
+++ Medium/1202. Smallest String With Swaps.py
@@ -1,1 +1,25 @@
 # Problem Statement: https://leetcode.com/problems/smallest-string-with-swaps/
+
+class Solution:
+    def smallestStringWithSwaps(self, s, pairs):
+        d = defaultdict(list)
+        for a,b in pairs:
+            d[a].append(b)
+            d[b].append(a)
+        #
+        def dfs(x,A):
+            if x in d:
+                A.append(x)
+                for y in d.pop(x):
+                    dfs(y,A)
+        #
+        s    = list(s)
+        while d:
+            x = next(iter(d))
+            A = []
+            dfs(x,A)
+            A = sorted(A)
+            B = sorted([ s[i] for i in A ])
+            for i,b in enumerate(B):
+                s[A[i]] = b
+        return ''.join(s)
