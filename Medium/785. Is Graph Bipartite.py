8fdb636a90398ad64b1584e5bd3abd475af30e06
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-04-29 23:50:57 +0530

    add 785. Is Graph Bipartite?

--- Medium/785. Is Graph Bipartite.py
+++ Medium/785. Is Graph Bipartite.py
@@ -1,1 +1,29 @@
 # Problem Statement: https://leetcode.com/problems/is-graph-bipartite/
+
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        def checkBipartite(vertex):
+            nonlocal color
+            nonlocal q
+
+            q.append(vertex)
+            color[vertex] = 1
+
+            while(q):
+                curr = q.popleft()
+
+                for i in graph[curr]:
+                    if color[i] == -1:
+                        color[i] = 1-color[curr]
+                        q.append(i)
+                    else:
+                        if color[i] == color[curr]:
+                            return False
+            return True
+        color = [-1]*len(graph)
+        q = collections.deque()
+        for i in range(len(graph)):
+            if color[i] == -1:
+                if not checkBipartite(i):
+                    return False
+        return True
