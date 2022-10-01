6944355ed968ac9b7071325d155b3fe82748581d
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-10-01 21:43:13 +0530

    add 412. Fizz Buzz

---
+++ Easy/412. Fizz Buzz.py
@@ -0,0 +1,15 @@
+# Problem Statement: https://leetcode.com/problems/fizz-buzz/
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        ans = []
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                ans.append("FizzBuzz")
+            elif i % 3 == 0:
+                ans.append("Fizz")
+            elif i % 5 == 0:
+                ans.append("Buzz")
+            else:
+                ans.append(str(i))
+        return ans
